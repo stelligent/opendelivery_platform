7b79cd9ccc8b5e40b49fff7b2fa5fbc117c49903
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-09 23:25:25 -0400

    removed extra aws file and updated deployment

--- deployment/config/deploy.rb
+++ deployment/config/deploy.rb
@@ -4,8 +4,19 @@ load File.expand_path('/usr/share/tomcat6/scripts/config/aws.config')
 
 sdb = AWS::SimpleDB.new
 
+def get_binding
+  binding # So that everything can be used in templates generated for the servers
+end
+
+def from_template(file)
+  require 'erb'
+  template = File.read(File.join(File.dirname(__FILE__), "..", file))
+  result = ERB.new(template).result(self.get_binding)
+end
+
 set :stack, ENV['stack']
 set :ssh_key, ENV['key']
+set :artifact, ENV['artifact']
 
 set :artifact_bucket do
   item = sdb.domains["stacks"].items["#{stack}"]
@@ -20,9 +31,10 @@ end
 set :user,             "ec2-user"
 set :use_sudo,         false
 set :deploy_to,        "/usr/share/tomcat6/webapps"
-set :artifact,         "wildtracks.war"
 set :artifact_url,     "https://s3.amazonaws.com/#{artifact_bucket}/#{artifact}"
-set :ssh_options,      { :forward_agent => true, :paranoid => false, :keys => ssh_key }
+set :ssh_options,      { :forward_agent => true, 
+                         :paranoid => false, 
+                         :keys => ssh_key }
 
 set :application, domain
 
@@ -32,26 +44,17 @@ role :db,  ip_address, :primary => true
 
 set :deploy_via, :remote_cache
 
-def get_binding
-  binding # So that everything can be used in templates generated for the servers
-end
-
-def from_template(file)
-  require 'erb'
-  template = File.read(File.join(File.dirname(__FILE__), "..", file))
-  result = ERB.new(template).result(self.get_binding)
-end
-
 namespace :deploy do
   
   task :setup do
     run "sudo chown -R tomcat:tomcat #{deploy_to}"
     run "sudo service httpd stop"
     run "sudo service tomcat6 stop"
+    run "sudo rm -rf #{deploy_to}/*"
   end
   
   task :deploy do
-    run "cd #{deploy_to} && sudo rm -rf wildtracks* && sudo wget #{artifact_url}"
+    run "cd #{deploy_to} && sudo wget #{artifact_url}"
   end
 
   task :restart, :roles => :app do
