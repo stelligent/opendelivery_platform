c3800ba4335cac2969c089d67ab858f7e8acacc6
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-14 04:21:23 -0400

    updated

--- config/aws/cd_security_group.rb
+++ config/aws/cd_security_group.rb
@@ -5,6 +5,7 @@ require 'trollop'
 load File.expand_path('/opt/aws/aws.config')
 
 ec2 = AWS::EC2.new
+sdb = AWS::SimpleDB.new
 
 opts = Trollop::options do
   opt :securityGroupName, "Name of security group", :short => "s", :type => String
@@ -16,3 +17,13 @@ security_group_id = security_group_name.id
 
 security_group = ec2.security_groups["#{security_group_id}"]
 security_group.authorize_ingress(:tcp, "#{opts[:port]}", security_group)
+
+AWS::SimpleDB.consistent_reads do
+  domain = sdb.domains["cdplatform"]
+  item = domain.items["properties"]
+  
+  item.attributes.add(
+    'SGID' => ,  "#{security_group}")
+end
+
+puts security_group
