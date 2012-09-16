8c752a7df8725df987b3b2738d08e88c52af1148
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-16 01:43:22 -0400

    added fork automation

---
+++ config/fork_github_repo.rb
@@ -0,0 +1,12 @@
+require 'github_api'
+require 'trollop'
+
+opts = Trollop::options do
+  opt :user, "Github Username", :short => "u", :type => String
+  opt :pass, "Github Password", :short => "p", :type => String
+end
+
+github = Github.new login: "#{opts[:user]}", password: "#{opts[:pass]}"
+
+github.repos.forks.create 'stelligent', 'continuous_delivery_open_platform'
+github.repos.forks.create 'stelligent', 'continuous_delivery_open_platform_jenkins_configuration'
