7b6657cc366d28e6dc357126d6443534fcaeaa4e
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-24 22:58:31 -0400

    added email script

---
+++ lib/add_email_to_group.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'aws-sdk'
+require 'trollop'
+
+load File.expand_path('/opt/aws/aws.config')
+
+sns = AWS::SNS.new
+
+opts = Trollop::options do
+  opt :email, "Email Address", :short => "e", :type => String
+  opt :snsarn, "Arn for SNS topic", :short => "a", :type => String
+end
+
+topic = sns.topics["#{opts[:snsarn]}"]
+topic.subscribe("#{opts[:email]}")
