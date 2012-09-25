93adb9270009bb86d82976230b0f0f102dd28a3e
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-24 22:26:53 -0400

    added itemname

--- lib/set_properties.rb
+++ lib/set_properties.rb
@@ -6,13 +6,14 @@ load File.expand_path('/opt/aws/aws.config')
 opts = Trollop::options do
   opt :property, "Name of property", :short => "p", :type => String
   opt :value, "Value of property", :short => "v", :type => String
+  opt :itemname, "SDB Item name", :short => "i", :type => String
 end
 
 sdb = AWS::SimpleDB.new
   
 AWS::SimpleDB.consistent_reads do
   domain = sdb.domains["stacks"]
-  item = domain.items["properties"]
+  item = domain.items["#{opts[:itemname]}"]
   
   item.attributes.set(
     "#{opts[:property]}" => ["#{opts[:value]}"])
