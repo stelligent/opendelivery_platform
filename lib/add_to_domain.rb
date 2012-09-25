33a44280b53e9bf4bcdab90f083da05e8905a0cb
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-24 20:01:47 -0400

    addded new script

---
+++ lib/add_to_domain.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'aws-sdk'
+require 'trollop'
+load File.expand_path('/opt/aws/aws.config')
+
+opts = Trollop::options do
+  opt :property, "Name of property", :short => "p", :type => String
+  opt :value, "Value of property", :short => "v", :type => String
+end
+
+sdb = AWS::SimpleDB.new
+  
+AWS::SimpleDB.consistent_reads do
+  domain = sdb.domains["stacks"]
+  item = domain.items["properties"]
+  
+  item.attributes.set(
+    "#{opts[:property]}" => ["#{opts[:value]}"])
+end
