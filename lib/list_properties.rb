1ecac9ff6a2d515922bd9f68e762b909186f94c3
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-24 21:06:58 -0400

    added list properties

---
+++ lib/list_properties.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'aws-sdk'
+require 'trollop'
+load File.expand_path('/opt/aws/aws.config')
+
+opts = Trollop::options do
+  opt :itemname, "Name of item", :short => "i", :type => String
+end
+
+sdb = AWS::SimpleDB.new
+  
+AWS::SimpleDB.consistent_reads do
+  domain = sdb.domains["stacks"]
+  item = domain.items["#{opts[:itemname]}"]
+  
+  item.attributes.each do |attribute|
+    puts attribute.name
+  end
+end
