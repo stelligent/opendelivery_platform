ad0288bea32e55d914a2cd1568c110939fc7903d
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-27 12:57:50 -0400

    automated puppet tar

--- lib/add_github_key.rb
+++ lib/add_github_key.rb
@@ -4,12 +4,13 @@ require 'trollop'
 opts = Trollop::options do
   opt :user, "Github Username", :short => "u", :type => String
   opt :pass, "Github Password", :short => "p", :type => String
+  opt :publicKey, "path to public key", :short => "k", :type => String
 end
 
 Github.new login: "#{opts[:user]}", password: "#{opts[:pass]}"
 keys = Github::Users::Keys.new
 
-file = File.open("/tmp/id_rsa.pub", "r")
+file = File.open("#{opts[:publicKey]}", "r")
 rsa = file.read
 
 keys.create "title" => "CDPlatform",
