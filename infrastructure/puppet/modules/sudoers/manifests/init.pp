8cecd8a83eec1115af21c9b802d2a4b9acf2ad16
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-28 15:04:18 -0400

    removed sudoers tempalte and added comments to system module

--- infrastructure/puppet/modules/sudoers/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class sudoers {
-  file { "/etc/sudoers":
-      content => template("sudoers/sudoers.erb"),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '440',
-  }
-}
--- infrastructure/puppet/modules/system/manifests/init.pp
+++ infrastructure/puppet/modules/system/manifests/init.pp
@@ -2,43 +2,11 @@ class system {
   
   include params
   
+  # Set paramteres from params.pp manifest
   $access_key = $params::access_key
   $secret_access_key = $params::secret_access_key
   
   Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
-  
-  package { "gcc":                 ensure => "installed" }
-  package { "mod_proxy_html":      ensure => "installed" }
-  package { "perl":                ensure => "installed" }
-  package { "libxslt-devel":       ensure => "installed" }
-  package { "libxml2-devel":       ensure => "installed" }
-  package { "make":                ensure => "installed" }
-  
-  package {"bundler":
-    ensure => "1.1.4", 
-    provider => gem
-  }
-  
-  package {"trollop":
-    ensure => "2.0", 
-    provider => gem
-  }
-  
-  package {"aws-sdk":
-    ensure => "1.5.6 ",
-    provider => gem,
-    require => [ 
-      Package["gcc"], 
-      Package["make"]
-      ]
-  }
-  
-  file { "/home/ec2-user/aws.config":
-      content => template("system/aws.config.erb"),
-      owner   => 'ec2-user',
-      group   => 'ec2-user',
-      mode    => '500',
-  }
   
   define download_file($site="",$cwd="",$creates=""){                                                                                         
     exec { $name:                                                                                                                     
@@ -46,6 +14,26 @@ class system {
       cwd => $cwd,
       creates => "${cwd}/${name}"                                                                                       
     }
+  }
+  
+  # Install basic packages
+  package { "gcc":                 ensure => "installed" }
+  package { "mod_proxy_html":      ensure => "installed" }
+  package { "perl":                ensure => "installed" }
+  package { "libxslt-devel":       ensure => "installed" }
+  package { "libxml2-devel":       ensure => "installed" }
+  package { "make":                ensure => "installed" }
+  
+  # Install basic gems
+  package { "bundler":             ensure => "1.1.4", provider => gem }
+  package { "trollop":             ensure => "2.0",   provider => gem }
+  package { "aws-sdk":             ensure => "1.5.6", provider => gem, require => [ Package["gcc"], Package["make"] ] }
+  
+  file { "/home/ec2-user/aws.config":
+      content => template("system/aws.config.erb"),
+      owner   => 'ec2-user',
+      group   => 'ec2-user',
+      mode    => '500',
   }
   
   exec {"authorized_keys":
