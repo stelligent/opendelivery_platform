e01b1e0e0b8f2cc73da68d23dd0cd721364b295d
Brian Jakovich <brian.jakovich@stelligent.com>
2012-09-28 23:12:21 -0400

    added ami creation script

---
+++ lib/create_ami.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'aws-sdk'
+require 'trollop'
+
+load File.expand_path('/opt/aws/aws.config')
+
+opts = Trollop::options do
+  opt :stackname, "Stack Name", :short => "s", :type => String
+  opt :imagename, "Name of image", :short =>  "i", :type => String
+end
+
+cfn = AWS::CloudFormation.new
+ec2 = AWS::EC2.new
+sdb = AWS::SimpleDB.new
+
+stack = cfn.stacks["#{opts[:stackname]}"]
+
+stack.resources.each do |resource|
+  if resource.resource_type == "AWS::EC2::Instance"
+    @instance = resource.physical_resource_id
+  end
+end
+
+image = ec2.images.create(:instance_id => "#{@instance}",
+                  :name => "#{opts[:imagename]}")
+                  
+while image.state != :available
+  sleep 10
+  case image.state
+  when :failed
+    image.delete
+    raise RuntimeError, 'Image Creation Failed'
+  end
+end
+
+AWS::SimpleDB.consistent_reads do
+  domain = sdb.domains["stacks"]
+  item = domain.items["ami"]
+  
+  item.attributes.set(
+    "latest" => "#{image.id}")
+end
--- lib/transient_environments.rb
+++ lib/transient_environments.rb
@@ -8,24 +8,24 @@ cfn = AWS::CloudFormation.new
 time = Time.new
 
 # Loop through all stacks on account
-cfn.stacks.each do |i|
+cfn.stacks.each do |stack|
   
   # Get current date and time
   t1 = Time.parse(time.getutc.to_s)
   # Get date and time CloudFormation stack was created at
-  t2 = Time.parse(i.creation_time.to_s)
+  t2 = Time.parse(stack.creation_time.to_s)
   
   # Find difference between current time and CloudFormation Stack creation time by 24 hour periods
   diff = (t1 - t2)/86400
   
   # Filter through Stacks for Target Instance stacks
-  if i.description == "Target"
-    i.parameters.each do |k,v|
+  if stack.description == "Target"
+    stack.parameters.each do |k,v|
       if k == "KeyName"
         if v == "development"
           if diff > 1
             # If the difference between current time and CloudFormation stack creation time is greater that 24 hours, delete the stack
-            i.delete
+            stack.delete
           end
         end
       end
