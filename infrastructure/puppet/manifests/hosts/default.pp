b49e7619804d8fefcdd67a41e34eb73a24b8a4b4
Brian Jakovich <brian.jakovich@stelligent.com>
2012-12-18 00:20:41 -0500

    updated default.pp

--- infrastructure/puppet/manifests/hosts/default.pp
+++ infrastructure/puppet/manifests/hosts/default.pp
@@ -1,8 +1,8 @@
 node default {
-  class { "params":     stage => pre  }
+
 	class { "java":       stage => pre  }
 	class { "system":     stage => pre  }
-	class { "tomcat6":    stage => main }
-	class { "postgresql": stage => main }
-	class { "httpd":      stage => main }
+	class { "passenger":  stage => main }
+	class { 'mysql':      stage => main }
+
 }
