a1d41cc15d35e58d38992c973effbeb1eea368ce
Brian Jakovich <brian.jakovich@stelligent.com>
2013-04-23 08:39:31 -0400

    updated deployment

--- deployment/config/deploy.rb
+++ deployment/config/deploy.rb
@@ -1,28 +1,19 @@
 task :preconfigure do
   run "cd #{deploy_to} && sudo rm -rf #{deploy_to}/#{artifact_name}"
+  run "mkdir #{artifact_name}"
   config_content = from_template("config/templates/s3_download.erb")
   put config_content, "/home/ec2-user/s3_download.rb"
   run "sudo chmod 655 /home/ec2-user/s3_download.rb"
 end
 
 task :update_code do
-  run "sudo ruby /home/ec2-user/s3_download.rb --outputdirectory #{deploy_to}/ --bucket #{s3_bucket} --key #{artifact}"
-  case
-  when language == "rails"
-    run "cd #{deploy_to} && sudo tar -zxf #{artifact}"
-    run "sudo chown -R ec2-user:ec2-user #{deploy_to}/#{artifact_name}"
-  end
+  run "sudo ruby /home/ec2-user/s3_download.rb --outputdirectory #{deploy_to}/#{artifact_name} --bucket #{s3_bucket} --key #{artifact}"
+  run "cd #{deploy_to}/#{artifact_name} && sudo tar -zxf #{artifact}"
+  run "sudo chown -R ec2-user:ec2-user #{deploy_to}/#{artifact_name}"
 end
 
 task :restart, :roles => :app do
-  case
-  when language == "rails"
-    run "sudo service httpd restart"
-
-  when language == "java"
-    run "sudo service httpd restart"
-    run "sudo service tomcat6 restart"
-  end
+  run "sudo service httpd restart"
 end
 
 task :postconfigure do
--- lib/search_and_replace_git_repo.rb
+++ lib/search_and_replace_git_repo.rb
@@ -1,30 +1,31 @@
-platform_file_names = ['/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/AddUserToNotificationGroup/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/BuildDeployedAMI/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/BuildInfrastructureAMI/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/CommonStepDefinitionGemBuild/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/CreateLoadBalancer/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/CreateTargetEnvironment/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/CreateTestDatabase/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/DeployApplication/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/ExportProductionDatabase/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/GetSSLArn/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/InfrastructureBuild/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/ListStacks/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/ShowDeliveryPipelineConfiguration/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/StartDeliveryPipeline/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/TerminateTargetEnvironment/config.xml',
-              '/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/UpdateDeliveryPipelineConfiguration/config.xml']
+repo = ARGV[0]
+platform_file_names = ['/usr/share/tomcat6/.jenkins/jobs/AddUserToNotificationGroup/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/BuildDeployedAMI/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/BuildInfrastructureAMI/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/CommonStepDefinitionGemBuild/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/CreateLoadBalancer/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/CreateTargetEnvironment/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/CreateTestDatabase/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/DeployApplication/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/ExportProductionDatabase/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/GetSSLArn/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/InfrastructureBuild/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/ListStacks/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/ShowDeliveryPipelineConfiguration/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/StartDeliveryPipeline/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/TerminateTargetEnvironment/config.xml',
+              '/usr/share/tomcat6/.jenkins/jobs/UpdateDeliveryPipelineConfiguration/config.xml']
 
-app_file_names = ['/Users/brianjakovich/Development/OPENDELIVERY/continuous_delivery_open_platform_jenkins_configuration/jobs/ApplicationBuild/config.xml']
+app_file_names = ['/usr/share/tomcat6/.jenkins/jobs/ApplicationBuild/config.xml']
 
 platform_file_names.each do |file_name|
   text = File.read(file_name)
-  output_of_gsub = text.gsub("git@github.com:stelligent/continuous_delivery_open_platform.git", "git@github.com:stelligent/stelligent_platform.git")
+  output_of_gsub = text.gsub("git@github.com:stelligent/continuous_delivery_open_platform.git", "git@github.com:stelligent/#{repo}_platform.git")
   File.open(file_name, "w") {|file| file.puts output_of_gsub}
 end
 
 app_file_names.each do |file_name|
   text = File.read(file_name)
-  output_of_gsub = text.gsub("git@github.com:stelligent/continuous_delivery_open_platform.git", "git@github.com:stelligent/stelligent.git")
+  output_of_gsub = text.gsub("git@github.com:stelligent/continuous_delivery_open_platform.git", "git@github.com:stelligent/#{repo}.git")
   File.open(file_name, "w") {|file| file.puts output_of_gsub}
 end
