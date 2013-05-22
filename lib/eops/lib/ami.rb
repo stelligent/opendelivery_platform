1eb118beebccb4259e22749e0bf1943ef86d135c
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-22 02:28:02 -0400

    Added AMI class

---
+++ lib/eops/lib/ami.rb
@@ -0,0 +1,34 @@
+class Ami
+
+  def initialize
+    @ec2 = AWS::EC2.new
+    @sdb = AWS::SimpleDB.new
+    @auto_scale = AWS::AutoScaling.new
+  end
+
+  def create_image(auto_scaling_group_name, image_name, sdb_domain, ami_type)
+    image = ec2.images.create(:instance_id => @auto_scale.groups[auto_scaling_group_name].auto_scaling_instances.first.id,
+                              :name => image_name)
+
+    sleep 10
+
+    while image.state != :available
+      sleep 10
+      case image.state
+      when :failed
+        image.delete
+        raise RuntimeError, 'Image Creation Failed'
+      end
+    end
+    update_simple_db(sdb_domain, ami_type, image.id)
+  end
+
+  def update_simple_db(sdb_domain, ami_type, image_id)
+    AWS::SimpleDB.consistent_reads do
+      domain = sdb.domains[sdb_domain]
+      item = domain.items["ami"]
+
+      item.attributes.set(ami_type => image_id)
+    end
+  end
+end
