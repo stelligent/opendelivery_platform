bbd2fe7f104a2c1da0e675c1d99fdec3454e5868
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-22 02:28:11 -0400

    Added upload class

---
+++ lib/eops/lib/upload.rb
@@ -0,0 +1,20 @@
+class S3Tools
+
+  @s3 = AWS::S3.new
+
+  def upload(file, bucket, key)
+    s3.buckets[bucket_name].objects[key].write(:file => file)
+  end
+
+  def download(bucket, key, output_directory)
+    obj = s3.buckets["#{opts[:bucket]}"].objects["#{opts[:key]}"]
+
+    base = Pathname.new("#{obj.key}").basename
+
+    Dir.mkdir(output_directory) unless File.exists?(output_directory)
+
+    File.open("#{output_directory}/#{base}", 'w') do |file|
+      file.write(obj.read)
+    end
+  end
+end
