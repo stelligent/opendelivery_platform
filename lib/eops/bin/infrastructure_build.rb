cbcb2f5e7b9c033d8df16ea6c462a6b67657fdb4
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-23 10:31:33 -0400

    Added Dir class to move around directories and removed trollop

--- lib/eops/bin/infrastructure_build.rb
+++ lib/eops/bin/infrastructure_build.rb
@@ -1,12 +1,7 @@
 require_relative "boot"
 
-opts = Trollop::options do
-  opt :key, "Key", :short => "k", :type => String
-  opt :item, "SDB Item name", :short => "i", :type => String
-  opt :sdbdomain, "Name of sdb domain", :short => "q", :type => String
-end
-
 workspace_dir = ARGV[0]
 
-system "cd #{workspace_dir}/infrastructure/puppet"
-system "tar -czvf puppet.tar.gz modules/* manifests/*"
+Dir.chdir("#{workspace_dir}/infrastructure/puppet") do
+  system "tar -czvf puppet.tar.gz modules/* manifests/*"
+end
