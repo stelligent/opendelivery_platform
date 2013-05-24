9cb0a34c486f4184612bfa0db623e459a20b9df3
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-23 20:04:26 -0400

    Added a create and delete method for instances and a find in autoscaling group instance

---
+++ lib/eops/lib/instance.rb
@@ -0,0 +1,17 @@
+require_relative "boot"
+
+class Instance
+  def initialize
+    @ec2 = AWS::EC2.new
+    @auto_scaling= AWS::AutoScaling.new
+  end
+
+  def destroy
+  end
+
+  def find_in_as_group(as_group_name)
+    first_instance = @auto_scaling.groups[as_group_name].auto_scaling_instances.first.id
+
+    return @ec2.instances[first_instance].ip_address
+  end
+end
