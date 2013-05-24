7c5cd255f2fdc4ef39188863beba2cd84ba3c077
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-23 20:05:07 -0400

    Added getters and setters for properties

---
+++ lib/eops/lib/domain.rb
@@ -0,0 +1,51 @@
+require_relative "boot"
+
+class Domain
+
+  def initialize
+    @sdb = AWS::SimpleDB.new
+  end
+
+  def create(domain_name)
+    AWS::SimpleDB.consistent_reads do
+      @sdb.domains.create(domain_name)
+    end
+  end
+
+
+  def delete(domain_name)
+    @sdb.domains[domain_name].delete
+  end
+
+  def load(file_name, sdb_domain, item_name)
+    file = File.open("/tmp/#{file_name}", "r")
+    AWS::SimpleDB.consistent_reads do
+      item = @sdb.domains[sdb_domain].items[item_name]
+      file.each_line do |line|
+        key,value = line.split '='
+        item.attributes.set( "#{key}" => "#{value}")
+      end
+    end
+  end
+
+
+  def get_property(sdb_domain, item_name, key)
+    AWS::SimpleDB.consistent_reads do
+    item = @sdb.domains[sdb_domain].items[item_name]
+
+      item.attributes.each_value do |name, value|
+        if name == key
+          puts value.chomp
+        end
+      end
+    end
+  end
+
+  def set_property(sdb_domain, item_name, property, value)
+    AWS::SimpleDB.consistent_reads do
+      item = @sdb.domains[sdb_domain].items[item_name]
+
+      item.attributes.set(property => [value])
+    end
+  end
+end
