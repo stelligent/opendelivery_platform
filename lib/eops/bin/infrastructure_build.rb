dd5483d9d642bdab7aeaf0bbc1ee0501be47f399
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-23 20:37:52 -0400

    Moved full infrastructure build into ruby file

--- lib/eops/bin/infrastructure_build.rb
+++ lib/eops/bin/infrastructure_build.rb
@@ -1,7 +1,19 @@
 require_relative "boot"
 
 workspace_dir = ARGV[0]
+sdb_domain = ARGV[1]
+application_name = ARGV[2]
+
+@domain = Domain.new
+@storage = Storage.new
+
+s3bucket = domain.get_property(sdb_domain, "properties", "S3Bucket")
+
 
 Dir.chdir("#{workspace_dir}/infrastructure/puppet") do
   system "tar -czvf puppet.tar.gz modules/* manifests/*"
 end
+
+@storage.upload("infrastructure/puppet/puppet.tar.gz", s3bucket, "puppet.tar.gz")
+
+@domain.set_property(sdb_domain, "properties", "ArtifactUrl", "https:://s3.amazonaws.com/#{s3bucket}/#{application_name}.tar.gz")
