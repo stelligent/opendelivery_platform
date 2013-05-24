15e76fc9cc1c51d95b42cffde7e227cf66455e6b
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-23 20:03:50 -0400

    Created a Keypair class

---
+++ lib/eops/lib/key_pair.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+class KeyPair
+  def initialize
+    @ec2 = AWS::EC2.new
+  end
+
+  def create(name)
+    key_pair = @ec2.key_pairs.create(name)
+    File.open("/tmp/#{name}", "w") do |f|
+      f.write(key_pair.private_key)
+    end
+  end
+
+  def delete(name)
+    @ec2.key_pairs[name].delete
+  end
+end
