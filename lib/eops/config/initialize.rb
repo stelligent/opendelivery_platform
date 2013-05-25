81b54e584bc9c65c18502460cb72ba04b581a1f7
Brian Jakovich <brian.jakovich@stelligent.com>
2013-05-25 00:23:12 -0400

    Added all libraries to initializer

--- lib/eops/config/initialize.rb
+++ lib/eops/config/initialize.rb
@@ -1,3 +1,15 @@
+require 'rubygems'
+require 'aws-sdk'
+require 'trollop'
+
+load File.expand_path('/opt/aws/aws.config')
+
 APP_PATH = File.expand_path('../../', __FILE__) + '/'
 
-require APP_PATH + 'lib/*.rb'
+require APP_PATH + 'lib/database.rb'
+require APP_PATH + 'lib/domain.rb'
+require APP_PATH + 'lib/image.rb'
+require APP_PATH + 'lib/instance.rb'
+require APP_PATH + 'lib/key_pair.rb'
+require APP_PATH + 'lib/stack_builder.rb'
+require APP_PATH + 'lib/storage.rb'
