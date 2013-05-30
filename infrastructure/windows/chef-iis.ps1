bcc20190435f6486334037e026df54f23f73827b
rich.mills <rich.mills@coveros.com>
2013-05-30 16:24:00 -0400

    Added msdeploy and 7zip to installer for IIS.

--- infrastructure/windows/chef-iis.ps1
+++ infrastructure/windows/chef-iis.ps1
@@ -1,5 +1,5 @@
 
-# Install IIS with Chef-solo
+# Install IIS and supporting utilities with Chef-solo
 
 $chef = "C:\chef"
 $opscode = "C:\opscode"
@@ -13,11 +13,12 @@ mkdir -force $chef\cookbooks
 
 $solo_rb = @"
 file_cache_path File.join(Dir.pwd, 'cache')
-cookbook_path File.join(Dir.pwd, 'cookbooks')
+cookbook_path [File.join(Dir.pwd, 'cookbooks'), File.join(Dir.pwd, 'cookbooks\WebTrends')]
 "@
 Set-Content -Value $solo_rb -Path $chef\solo.rb
 
 
+# Install IIS, msdeploy, 7zip, .NET 4.5 to build an IIS server
 $solo_js = @"
 {
   "iis": {
@@ -26,7 +27,9 @@ $solo_js = @"
 
   "run_list": [
     "recipe[iis]",
-    "recipe[iis::mod_deploy"
+    "recipe[iis::mod_deploy]"
+    "recipe[7-zip]", 
+    "recipe[ms_dotnet45]"
   ]
 }
 "@
@@ -40,5 +43,9 @@ git clone git://github.com/opscode-cookbooks/iis.git
 git clone git://github.com/opscode-cookbooks/webpi.git
 git clone git://github.com/opscode-cookbooks/windows.git
 git clone git://github.com/opscode-cookbooks/chef_handler.git
+git clone https://github.com/opscode-cookbooks/7-zip
+git clone https://github.com/Webtrends/Cookbooks WebTrends
+
+
 cd $chef
 chef-solo -c $chef\solo.rb -j $chef\solo.js -l debug
