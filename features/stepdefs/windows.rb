155fbd3d1a2c4a34464d7f00629e931706241113
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-06-07 15:17:42 -0400

    fleshing out

--- features/stepdefs/windows.rb
+++ features/stepdefs/windows.rb
@@ -1,39 +1,19 @@
-require 'open3'
+# require "PowerShellRunner.rb"
 
-class PowerShellRunner
+# remote_command = "dir"
+# hostname = "localhost"
+# username = "Administrator"
+# password = "EVYA)SKxy?"
 
-    def initialize hostname, username, password, remote_command
-        @hostname = hostname
-        @username = username
-        @password = password
-        @remote_command = remote_command
-    end
+# psr = PowerShellRunner.new hostname, username, password, remote_command
+# out, err = psr.execute
+# puts out
+# puts err
 
-    def execute
-        std_out = ""
-        std_err = ""
-        command = "
-$username='#{@username}'
-$password='#{@password}'
-$pass = ConvertTo-SecureString -AsPlainText $password -Force
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $pass
-Invoke-Command -ComputerName localhost -ScriptBlock { #{@remote_command} } -credential $cred
-"
-        powershell_command = "powershell -command \"& {#{command}}\""
-        stdin, stdout, stderr = Open3.popen3(powershell_command) 
-        stdout.each_line { |line| std_out += line}
-        stderr.each_line { |line| std_err += line}
-
-        return std_out, std_err
-    end
+When(/^I run "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
 end
 
-remote_command = "dir"
-hostname = "localhost"
-username = "Administrator"
-password = "EVYA)SKxy?"
-
-psr = PowerShellRunner.new hostname, username, password, remote_command
-out, err = psr.execute
-puts out
-puts err
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
