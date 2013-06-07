ce2c096aa927fc6140a033b03f58194c25614ac9
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-06-07 13:27:34 -0400

    adding initial cut of the windows cucumber tests

---
+++ features/stepdefs/PowerShellRunner.rb
@@ -0,0 +1,29 @@
+require 'open3'
+
+class PowerShellRunner
+
+    def initialize hostname, username, password, remote_command
+        @hostname = hostname
+        @username = username
+        @password = password
+        @remote_command = remote_command
+    end
+
+    def execute
+        std_out = ""
+        std_err = ""
+        command = "
+$username='#{@username}'
+$password='#{@password}'
+$pass = ConvertTo-SecureString -AsPlainText $password -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $pass
+Invoke-Command -ComputerName localhost -ScriptBlock { #{@remote_command} } -credential $cred
+"
+        powershell_command = "powershell -command \"& {#{command}}\""
+        stdin, stdout, stderr = Open3.popen3(powershell_command) 
+        stdout.each_line { |line| std_out += line}
+        stderr.each_line { |line| std_err += line}
+
+        return std_out, std_err
+    end
+end
---
+++ features/windows.feature
@@ -0,0 +1,21 @@
+Feature: Scripted Deployment of an Application
+    As a Developer
+    I would like my Deployment to be successful
+    so I can use the application
+
+    Background:
+        Given I am sshed into the environment
+
+    Scenario: Is the IIS service running?
+        When I run "ps -ef | grep httpd"
+
+
+    Scenario Outline: These files should be present
+        When I run "sudo ls -las <file>"
+        Then I should see "<file>"
+
+        Examples: file that should exist
+        |file     |
+        |/etc/httpd/conf/httpd.conf|
+
+
