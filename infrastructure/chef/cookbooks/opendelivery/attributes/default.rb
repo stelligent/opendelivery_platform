83792aaaeed34f97602bd305789d17eebcf9f75a
Brian Jakovich <brian.jakovich@stelligent.com>
2013-06-10 16:56:04 -0400

    added metadata.rb and fixed windows pkg

--- infrastructure/chef/cookbooks/opendelivery/attributes/default.rb
+++ infrastructure/chef/cookbooks/opendelivery/attributes/default.rb
@@ -1,6 +1,6 @@
-default['tomcat']['url'] = "http://apache.petsads.us/tomcat/tomcat-7/v7.0.40/bin/apache-tomcat-7.0.40.exe"
-default['tomcat']['home'] = "C:\tomcat"
-default['tomcat']['options'] = "/D=C:\Apache"
+default['tomcat']['url'] = 'http://apache.petsads.us/tomcat/tomcat-7/v7.0.40/bin/apache-tomcat-7.0.40.exe'
+default['tomcat']['home'] = 'C:\tomcat'
+default['tomcat']['options'] = '/D=C:\tomcat'
 
 
 default['ruby']['url'] = "http://rubyforge.org/frs/download.php/76952/rubyinstaller-1.9.3-p429.exe"
