a2f6a9eba22891d0ca4c058225dfbf8b5d9974c5
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-06-10 22:54:05 -0400

    added stepdefs for testing windows services

--- features/stepdefs/windows.rb
+++ features/stepdefs/windows.rb
@@ -10,10 +10,24 @@
 # puts out
 # puts err
 
-When(/^I run "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+
+Given(/^a server set up at "(.*?)"$/) do |arg|
+  @hostname = arg
 end
 
-Then(/^I should see "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^a user named "(.*?)"$/) do |arg|
+  @username = arg
+end
+
+Given(/^a password of "(.*?)"$/) do |arg|
+  @password = arg
+end
+
+When(/^I run "(.*?)"$/) do |remote_command|
+  runner = PowerShellRunner.new @hostname, @username, @password, remote_command
+  @std_out, @std_err = runner.execute
+end
+
+Then(/^I should see "(.*?)"$/) do |message|
+  @std_out.include?(message).should_not == false
 end
