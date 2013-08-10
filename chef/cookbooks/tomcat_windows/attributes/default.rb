eb0d14a615abb07c0bbaeb2588fd4bf987550d0d
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-10 15:44:53 -0400

    Moved from tomcat_windows to tomcat

--- chef/cookbooks/tomcat_windows/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default['tomcat']['s3']['bucket'] = node['aws']['s3']['bucket']
-default['tomcat']['windows']['name'] = "Apache Tomcat 7"
-default['tomcat']['windows']['home'] = 'C:\tomcat'
-default['tomcat']['windows']['service'] = 'Tomcat7'
-default['tomcat']['windows']['options'] = "/S /D=#{node['tomcat']['windows']['home']}"
-
-default['tomcat']['windows']['file'] = 'apache-tomcat-7.0.40.exe'
-default['tomcat']['windows']['key'] = "3rdParty/#{node['tomcat']['windows']['file']}"
-default['tomcat']['windows']['path'] = "Z:\\#{node['tomcat']['windows']['file']}"
