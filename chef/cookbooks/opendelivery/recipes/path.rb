31a6a6180f410d8c3efb5fad68dc0d1e591c7aba
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-10 15:44:35 -0400

    Added platform case to decide which chef resource to build

--- chef/cookbooks/opendelivery/recipes/path.rb
+++ chef/cookbooks/opendelivery/recipes/path.rb
@@ -1,7 +1,21 @@
-node['opendelivery']['config'].each do |var, path|
-  windows_batch "set environment variables" do
-    code <<-EOH
-    setx #{var} "#{path}" /m
-    EOH
+case node['platform']
+when "windows"
+
+  node['opendelivery']['config'].each do |key, value|
+    windows_batch "set environment variables" do
+      code <<-EOH
+        setx #{key} "#{value}" /m
+      EOH
+    end
+  end
+
+else
+
+  node['opendelivery']['config'].each do |key, value|
+    execute "set environment variables" do
+      code <<-EOH
+        echo "export #{key}=#{value}" >> /etc/sysconfig/tomcat6
+      EOH
+    end
   end
 end
--- chef/cookbooks/opendelivery/recipes/setup.rb
+++ chef/cookbooks/opendelivery/recipes/setup.rb
@@ -1,14 +1,25 @@
+case node['platform']
+when "windows"
+
+  windows_batch "Clone platform repo" do
+    code <<-EOH
+      set PATH=%PATH%;C:\\Program Files (x86)\\Git\\bin
+      git clone "https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['platform']['repo']}.git" Z:\\
+    EOH
+  end
+
+else
+
+ execute "Clone platform repo" do
+    code <<-EOH
+     git clone "https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['platform']['repo']}.git" /tmp/
+    EOH
+  end
+end
+
 aws_simpledb_create node['opendelivery']['domain'] do
   domain node['opendelivery']['domain']
   action :create
-end
-
-windows_batch "Clone platform repo" do
-  code <<-EOH
-  Z:
-  set PATH=%PATH%;C:\\Program Files (x86)\\Git\\bin
-  git clone #{node['opendelivery']['platform']['repo']}
-  EOH
 end
 
 aws_simpledb_load "Load initial configuration" do
