2b99c32a2da922986654cea1e447cdc4d606cb11
Brian Jakovich <brian.jakovich@stelligent.com>
2013-09-06 12:35:02 -0400

    added dependency on amazon instead of aws

--- chef/cookbooks/opendelivery/metadata.rb
+++ chef/cookbooks/opendelivery/metadata.rb
@@ -3,10 +3,9 @@ maintainer        "Brian Jakovich"
 maintainer_email  "brian.jakovich@stelligent.com"
 license           "Apache 2.0"
 description       "Installs and configures Open Delivery implementations"
-
 version           "1.0.0"
 recipe            "opendelivery", "Main installation"
 
-%w{ powershell git tomcat aws windows }.each do |cb|
+%w{ powershell git tomcat-overrides amazon windows }.each do |cb|
   depends cb
 end
