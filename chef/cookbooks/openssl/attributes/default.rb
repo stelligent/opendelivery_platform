cdca653741911de64af896d4cc10d300f7e6bb8b
Eric Kascic <eric.kascic@stelligent.com>
2013-09-06 13:37:44 -0400

    reverting to community cookbook

--- chef/cookbooks/openssl/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default['openssl']['file'] = 'openssl-0.9.8k_X64.zip'
-default['openssl']['key'] = "3rdParty/#{node['openssl']['file']}"
-default['openssl']['path'] = "Z:\\#{node['openssl']['file']}"
-default['openssl']['bucket'] =  'AdventResources'
