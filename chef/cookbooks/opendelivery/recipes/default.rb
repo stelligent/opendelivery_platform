f56c9598c5582bdfce6258dcda51a15e1d5f7ce4
Eric Kascic <eric.kascic@stelligent.com>
2013-09-06 14:08:36 -0400

    this is dead code

--- chef/cookbooks/opendelivery/recipes/default.rb
+++ chef/cookbooks/opendelivery/recipes/default.rb
@@ -18,16 +18,3 @@ execute "Setup jenkins repo" do
   git clone https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['jenkins']['repo']['name']}.git #{node['tomcat']['home']}/.jenkins
   EOH
 end
-
-ruby_block "Update config.xml for each jenkins job" do
-  block do
-
-    job_config_files = Dir["#{node['tomcat']['home']}/.jenkins/jobs/**/config.xml"]
-    job_config_files.each do |config|
-      text = File.read(config)
-      output_of_gsub = text.gsub('git@github.com:stelligent/continuous_delivery_open_platform.git', "https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['platform']['repo']['name']}.git" )
-      File.open(config, "w") {|file| file.puts output_of_gsub}
-    end
-  end
-  notifies :restart, "service[\"tomcat#{node['tomcat']['base_version']}\"]"
-end
