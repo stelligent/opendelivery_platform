013b31ce091f1c652ac274f477fc8c10ac75d2da
Brian Jakovich <brian.jakovich@stelligent.com>
2013-09-06 12:34:44 -0400

    Added new repo name variable to default recipe

--- chef/cookbooks/opendelivery/recipes/default.rb
+++ chef/cookbooks/opendelivery/recipes/default.rb
@@ -1,3 +1,8 @@
+aws_s3file node['jenkins']['path'] do
+  key node['jenkins']['key']
+  bucket node['opendelivery']['bucket']
+  action :create
+end
 
 directory "#{node['tomcat']['home']}/.ssh" do
   action :create
@@ -10,7 +15,7 @@ end
 
 execute "Setup jenkins repo" do
   command <<-EOH
-  git clone https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['jenkins']['repo']}.git #{node['tomcat']['home']}/.jenkins
+  git clone https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['jenkins']['repo']['name']}.git #{node['tomcat']['home']}/.jenkins
   EOH
 end
 
@@ -20,7 +25,7 @@ ruby_block "Update config.xml for each jenkins job" do
     job_config_files = Dir["#{node['tomcat']['home']}/.jenkins/jobs/**/config.xml"]
     job_config_files.each do |config|
       text = File.read(config)
-      output_of_gsub = text.gsub('git@github.com:stelligent/continuous_delivery_open_platform.git', "https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['platform']['repo']}.git" )
+      output_of_gsub = text.gsub('git@github.com:stelligent/continuous_delivery_open_platform.git', "https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['platform']['repo']['name']}.git" )
       File.open(config, "w") {|file| file.puts output_of_gsub}
     end
   end
