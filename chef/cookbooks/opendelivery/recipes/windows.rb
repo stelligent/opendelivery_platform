d6764c0cc539d9d6f0377c3031d40dba725b4379
Brian Jakovich <brian.jakovich@stelligent.com>
2013-09-14 23:03:03 -0400

    Removed deprecated functionality and changed from s3 download to using remote_file

--- chef/cookbooks/opendelivery/recipes/windows.rb
+++ chef/cookbooks/opendelivery/recipes/windows.rb
@@ -1,8 +1,7 @@
 path = win_friendly_path( ::File.join( node['tomcat']['home'],'.jenkins'))
 
-amazon_s3file node['jenkins']['path'] do
-  key node['jenkins']['key']
-  bucket node['opendelivery']['bucket']
+remote_file node['jenkins']['path'] do
+  source node['jenkins']['url']
   action :create
 end
 
@@ -27,17 +26,4 @@ windows_batch "Setup jenkins repo" do
   set PATH=%PATH%;C:\\Program Files (x86)\\Git\\bin
   git clone https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['jenkins']['repo']}.git #{path}
   EOH
-end
-
-ruby_block "Update config.xml for each jenkins job" do
-  block do
-
-    job_config_files = Dir["#{node['tomcat']['home']}\\.jenkins\\jobs\\**\\config.xml"]
-    job_config_files.each do |config|
-      text = File.read(config)
-      output_of_gsub = text.gsub('git@github.com:stelligent/continuous_delivery_open_platform.git', "https://#{node['git']['username']}:#{node['git']['password']}@github.com/#{node['git']['org']}/#{node['git']['platform']['repo']}.git" )
-      File.open(config, "w") {|file| file.puts output_of_gsub}
-    end
-  end
-  notifies :restart, "service[#{node['tomcat']['service']}]"
 end
